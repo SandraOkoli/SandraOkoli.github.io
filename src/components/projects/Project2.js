b80f60780e877e541562f93570e8d7cd75f5d437
SandraOkoli <sandraokoli@hotmail.co.uk>
2018-01-22 22:28:08 +0000

    Amended project 2

--- src/components/projects/Project2.js
+++ src/components/projects/Project2.js
@@ -7,14 +7,14 @@ const Project2 = () =>  {
       <hr/>
       <div className="row">
         <div className="col-md-6">
-          <img className="img-responsive"     src="http://www.bigredsawmill.com/wp-content/plugins/LayerSlider_wp_3.6/sampleslider/home-imac.png"/>
+          <img className="img-responsive" src="https://image.ibb.co/mQrD1w/Out_mac.png"/>
         </div>
         <div className="row">
           <div className="col-md-6">
             <h3>What did I make?</h3>
             <p>For our group project we decided to build an application that plans your night out.</p>
             <h3>What technologies were used?</h3>
-            <p>This was a team project that involved using Github collaboratively to produce a RESTful MEAN stack app. We utilised Google Maps API and Google Placesto enable users to search for a location when creating an events. I was tasked with testing (using Mocha, Chai and Istanbul),implementing the Angular moments and comments functionality, created two of the RESTful routes and styled the show page.</p>
+            <p>This was a team project that involved using Github collaboratively to produce a RESTful MEAN stack app. We utilised Google Maps API and Google Placesto enable users to search for a location when creating an events. I was tasked with testing (using Mocha, Chai and Istanbul), implementing the comments functionality, created two of the RESTful routes and styled the index and show page.</p>
             <div className="logos">
               <i className="devicon-mongodb-plain-wordmark"></i>
               <i className="devicon-express-original-wordmark"></i>
@@ -24,7 +24,7 @@ const Project2 = () =>  {
               <i className="devicon-gulp-plain"></i>
               <i className="devicon-bower-plain-wordmark"></i>
             </div>
-            <a href="#">Launch Project</a>
+            <a href="https://out-sandra.herokuapp.com/">Launch Project</a>
             <a href="https://github.com/SandraOkoli/WDI_GROUP_PROJECT">View Code</a>
           </div>
         </div>
